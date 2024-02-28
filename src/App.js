5564b01d443d5eb927ef229a319f5809992e1933
ib22122 <lalsalm53@gmail.com>
2024-02-28 18:36:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
+
+  const [x, setx] = useState([])
+  
+  const inputRef = useRef()
+
+  const add = () => {
+    const value = inputRef.current.value
+    const newData = { completed: false, value }
+    setx([...x, newData])
+
+    inputRef.current.value = ""
+  }
+
+  const toDelete = (index) => {
+    const newx = [...x]
+    newx.splice(index, 1)
+
+    setx(newx)
+  }
+  const itemDone = (index) => {
+
+    const newx = [...x]
+
+    newx[index].completed = !newx[index].completed
+
+    setx(newx)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h2>To do List</h2>
+        <div className='tasks'>
+          {
+            x.map(({ value, completed }, index) => {
+              return <div className='task' key={index}>
+                <div className={completed ? "col-10 tasktext diffstyle" : "col-10 tasktext"}>
+                  {value}
+                </div>
+                <div className=' col-2 spans '>
+                  <span onClick={() => itemDone(index)} >{completed ? "✅ " : "⬜ " }</span>
+                  <span onClick={() => toDelete(index)} >❌</span>
+                </div>
+              </div>
+            })
+          }
+        </div>
+
+        <input ref={inputRef} placeholder='Enter new task...' />
+
+        <button onClick={add} >Add</button>
+
+      </div>
     </div>
   );
 }
